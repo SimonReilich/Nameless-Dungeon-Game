ee9965bc41cd029493656909a94bcfa9daaf4906
sofiiaaleksandrovych <sofiia.eu@gmail.com>
2025-04-29 12:02:02 +0200

    gdjagdjuag

--- lwjgl3/src/main/java/io/github/simonreilich/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/simonreilich/lwjgl3/Lwjgl3Launcher.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import io.github.simonreilich.Game;
 
-/** Launches the desktop (LWJGL3) application. */
+/** Launches the desktop (LWJGL3) application.sgjadsdga@ */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
