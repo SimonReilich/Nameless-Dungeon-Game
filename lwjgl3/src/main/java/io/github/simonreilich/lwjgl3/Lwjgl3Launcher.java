9963e87ab29fa74c1a14f5d6ab39f428364bd4ad
SimonReilich <174124874+SimonReilich@users.noreply.github.com>
2025-07-26 13:46:21 +0200

    Name Update: now Dungeonfall - No Light Below

--- lwjgl3/src/main/java/io/github/simonreilich/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/simonreilich/lwjgl3/Lwjgl3Launcher.java
@@ -1,12 +1,11 @@
 package io.github.simonreilich.lwjgl3;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import io.github.simonreilich.View;
 
-/**
- * Launches the desktop (LWJGL3) application.
- */
+/** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
@@ -19,7 +18,7 @@ public class Lwjgl3Launcher {
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("SchoolGame");
+        configuration.setTitle("Dungeonfall - No Light Below");
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         configuration.useVsync(true);
@@ -29,10 +28,11 @@ public class Lwjgl3Launcher {
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+
         configuration.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("icon128.png", "icon64.png", "icon32.png", "icon16.png");
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
 }
